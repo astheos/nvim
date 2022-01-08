3c3a66c927b21b7b4dc6a59a2b721407bf9042ab
theos <ma.theos.gui@gmail.com>
2022-01-08 17:43:31 -0300

    add file with set of general options on neovim

---
+++ lua/config/setup.lua
@@ -0,0 +1,19 @@
+--------------- Setup Configuration ---------------
+
+require('interface')
+
+set('number', true)
+set('relativenumber', true)
+set('cursorline', true)
+set('termguicolors', true)
+set('showmode', false)
+
+set('tabstop', 4)
+set('softtabstop', 4)
+set('shiftwidth', 4)
+set('expandtab', false)
+
+set('timeout', true)
+set('timeoutlen', 60000)
+
+execute('syntax enable')
